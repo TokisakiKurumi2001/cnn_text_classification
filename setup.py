ee3a286bcb9b0fdeab7591da757fb7d5662d2b9c
TokisakiKurumi2001 <minhkhoile2001nsg@gmail.com>
2022-10-27 22:25:44 +0700

    Fix the bug of validation epoch end

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='cnn_text_classification',
     packages=['cnn_text_classification'],
-    version='0.1.0',
+    version='0.1.1',
     description='CNN Text Classification',
     author='Le Minh Khoi',
     license='MIT',
